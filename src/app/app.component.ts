2068a0bb5c0cc8b2020fdacb56effe6763537ad1
freshbrashman <freshbrashman@gmail.com>
2017-05-13 22:41:31 +0900

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FirebaseService} from './firebase.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor(firebaseService: FirebaseService) {
+    firebaseService.saveMessage("abcde");
+  }
 }
