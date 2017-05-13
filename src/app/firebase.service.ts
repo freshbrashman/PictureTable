a6c22dde300f56f54c6b0b5998f41532726f9986
freshbrashman <freshbrashman@gmail.com>
2017-05-13 22:41:31 +0900

    first commit

---
+++ src/app/firebase.service.ts
@@ -0,0 +1,69 @@
+//ｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰ
+// 通信処理
+//ｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰｰ
+import {Injectable} from "@angular/core";
+import "rxjs/add/operator/map";
+import { Observable, Subject, BehaviorSubject, ReplaySubject } from 'rxjs/Rx';
+import {initializeApp, auth, database, storage, User } from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyCf6FraRT1wqxHu4mh3bwS-ba0fK497MPk",
+    authDomain: "teru-kensho.firebaseapp.com",
+    databaseURL: "https://teru-kensho.firebaseio.com",
+    projectId: "teru-kensho",
+    storageBucket: "teru-kensho.appspot.com",
+    messagingSenderId: "604840872764"
+};
+
+@Injectable()
+export class FirebaseService {
+    private auth: auth.Auth;
+    private database: database.Database;
+    private storage: storage.Storage;
+    private messagesRef: database.Reference;
+    private informStableSubject$: Subject<boolean>;
+    private authSubject$: Subject<firebase.User | null>;
+    private currentUser: User;
+
+    constructor() {
+        initializeApp(config);
+
+        this.authSubject$ = new BehaviorSubject<firebase.User | null>(null);
+        this.informStableSubject$ = new ReplaySubject<boolean>();
+
+        this.auth = auth();
+        this.database = database();
+        this.storage = storage();
+
+        // サインインかサインアウトをするとその都度この処理が走る。
+        this.auth.onAuthStateChanged((user: firebase.User) => {
+        if (user) { // User is signed in!
+            this.authSubject$.next(user);
+            this.saveMessage('xxxxxxxxxxx');
+        } else { // User is signed out!
+            this.authSubject$.next(null);
+        }
+        });
+
+        this.messagesRef = this.database.ref('messages');
+        
+    }
+
+  saveMessage(message: string) {
+    if (message) {
+      const currentUser = this.auth.currentUser;
+      if (currentUser) {
+        // Add a new message entry to the Firebase Database.
+        this.messagesRef.push({
+          name: currentUser.displayName,
+          text: message,
+          photoUrl: currentUser.photoURL || '/images/profile_placeholder.png'
+        }).then(() => {
+          this.informStableSubject$.next(true);
+        }).catch((error) => {
+          console.error('Error writing new message to Firebase Database', error);
+        });
+      }
+    }
+  }    
+}
